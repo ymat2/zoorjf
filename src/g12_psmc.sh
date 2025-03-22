97a6bdbc0805b7d1f70e0b7a3c97cad857a1e6d0
ymat2 <ymat2@users.noreply.github.com>
2025-03-22 14:21:34 +0900

    :running: Run PSMC

---
+++ src/g12_psmc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH -a 1-130
+#SBATCH --mem 32G
+
+
+samples=($(ls bam | sort -V))
+sample=${samples[$SLURM_ARRAY_TASK_ID-1]}
+
+workdir=~/RJF/psmc
+reference=~/ref/grcg7b/GCF_016699485.2.fa
+
+shopt -s expand_aliases
+alias bcftools="apptainer exec /usr/local/biotools/b/bcftools:1.18--h8b25389_0 bcftools"
+alias psmc="apptainer exec /usr/local/biotools/p/psmc:0.6.5--h43eeafb_2"
+
+[ ! -e ${workdir}/${sample} ] && mkdir -p ${workdir}/${sample}
+cd ${workdir}
+
+bcftools consensus -f ${reference} ~/RJF/bam/${sample}/${sample}.q.vcf.gz -I | gzip > ${sample}/${sample}.consensus.fa.gz
+psmc fq2psmcfa -s 50 ${sample}/${sample}.consensus.fa.gz > ${sample}/${sample}.consensus.psmcfa
+psmc splitfa ${sample}/${sample}.consensus.psmcfa > ${sample}/${sample}.split.filtered.psmcfa
+psmc psmc -N25 -t15 -r5 -p "4+25*2+4+6" -o ${sample}/${sample}.psmc ${sample}/${sample}.split.filtered.psmcfa
+
+rm ${sample}/${sample}.consensus.fa.gz ${sample}/${sample}.consensus.psmcfa ${sample}/${sample}.split.filtered.psmcfa
