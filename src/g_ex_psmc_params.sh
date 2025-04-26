400c7dfd117582749da38f8971b790713548f11b
ymat2 <ymat2@users.noreply.github.com>
2025-04-26 17:52:00 +0900

    :wrench: Separate atomc times

--- src/g_ex_psmc_params.sh
+++ src/g_ex_psmc_params.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-#SBATCH -a 1-130
-#SBATCH --partition medium
-#SBATCH --mem 64G
+#SBATCH -a 1-10
+#SBATCH --mem 32G
 
 
-samples=($(ls bam | sort -V))
-sample=${samples[$SLURM_ARRAY_TASK_ID-1]}
+sample=TMP0001M
 
-workdir=~/RJF/psmc
+workdir=~/RJF/psmc_param
 reference=~/ref/grcg7b/GCF_016699485.2.fa
 
 shopt -s expand_aliases
@@ -27,3 +25,6 @@ psmc psmc -N25 -t15 -r5 -p "4+25*2+4+6" -o ${sample}/${sample}.4.psmc ${sample}/
 psmc psmc -N25 -t15 -r5 -p "2+2+25*2+4+6" -o ${sample}/${sample}.22.psmc ${sample}/${sample}.split.filtered.psmcfa
 psmc psmc -N25 -t15 -r5 -p "1+1+1+1+25*2+4+6" -o ${sample}/${sample}.1111.psmc ${sample}/${sample}.split.filtered.psmcfa
 rm ${sample}/${sample}.split.filtered.psmcfa
+
+#perl ../src/psmc_plot.pl -s 50 -X 50000000 -p -g 1 -R -x 1000 -u 1.91e-9 ${sample}/${sample}.plot ${sample}/${sample}.22.psmc
+#perl ../src/psmc_plot.pl -s 50 -X 50000000 -p -g 1 -R -x 1000 -u 1.91e-9 ${sample}/${sample}.plot ${sample}/${sample}.1111.psmc
